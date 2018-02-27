3c727b0a6b53e6d61dea40d7da76b769c5a5935f
cjf96 <cjf96@uw.edu>
2018-02-27 10:09:18 -0800

    get rid of test msg in index.js

--- auth/src/index.js
+++ auth/src/index.js
@@ -4,23 +4,21 @@ import App from './App';
 
 // Import additional libraries/styles
 // Adds bootstrap library and makes it look nice
-import firebase from'firebase/app';
+import firebase from 'firebase/app';
 import 'bootstrap/dist/css/bootstrap.css';
-
-let string = "this is a test";
 
 
 // Define your configuration for Firebase
-  // Initialize Firebase
-  var config = {
+// Initialize Firebase
+var config = {
     apiKey: "AIzaSyD7y6JWVwvNdw_rutCAch4DN_VTWHVXjzY",
     authDomain: "uw-endeavour.firebaseapp.com",
     databaseURL: "https://uw-endeavour.firebaseio.com",
     projectId: "uw-endeavour",
     storageBucket: "uw-endeavour.appspot.com",
     messagingSenderId: "170411021679"
-  };
-  firebase.initializeApp(config);
+};
+firebase.initializeApp(config);
 
 // Initialize the firebase app using the config
 
